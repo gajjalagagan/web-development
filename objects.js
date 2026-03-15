fe9f5abe50f936190b57c13631db3468398b0fa9
gajjalagagan <gajjalagagan82@gmail.com>
2026-03-15 19:35:17 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,23 @@
+ charan={
+    name:"wri charan",
+    age:15,
+    skills:"Hockey",
+}
+nihal={
+    name:"qihal arya",
+    age:14,
+    skills:"Throw ball"
+}
+vivek={
+    name:"eivekananda",
+    age:13,
+    skills:"Robotics"
+}
+friends=[vivek.name,nihal.name,charan.name]
+
+
+vivek.name="vivekananda"
+nihal.name="nihal arya"
+charan.name="sri charan"
+friends=[vivek.name,nihal.name,charan.name]
+console.log(friends)
