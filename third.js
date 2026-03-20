c0c955e0b5a53753c84d1203da01b66ec8d9c22f
gajjalagagan <gajjalagagan82@gmail.com>
2026-03-20 19:28:27 +0530

    Add files via upload

---
+++ third.js
@@ -0,0 +1,19 @@
+myfriends={
+    name:"Sri charan",
+    age:14,
+    skills:"vocals",
+    hobby:"singing",
+    intro:function(){
+        console.log(
+            `details about my friends:
+            =======================================
+            my friends name is${myfriends.name}
+            my friends age is${myfriends.age}
+            my friend's skills are ${myfriends.skills}
+            my friend's hobby is ${myfriends.hobby}`
+
+        )
+    }
+
+}
+myfriends.intro()
