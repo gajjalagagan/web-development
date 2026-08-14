26c65924221cd8993daaadcae812dfb19fe589cb
gajjalagagan <gajjalagagan82@gmail.com>
2026-08-14 19:15:30 +0530

    Add files via upload

---
+++ activit-10.py
@@ -0,0 +1,14 @@
+a = 56
+b = 7
+#swapping the  number
+a = a+b
+b = a-b
+a = a-b
+print("After swapping: a =", a, "b =", b)
+
+#using left shift doubling th number
+print("3<<1 =", 3<<1)
+print("3<<2=", 3<<2)
+print("3<<3=", 3<<3)
+print("3<<4=", 3<<4)
+print("3<<5=", 3<<5)
